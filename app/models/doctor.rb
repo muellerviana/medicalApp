8539ad2b51c261eca73e83bf50144e8bc6c2f2c6
muellerviana <muellerviana@gmail.com>
2023-07-14 21:57:43 -0300

    fix doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,11 @@
 class Doctor
   include Mongoid::Document
   include Mongoid::Timestamps
+  field :firstname, type: String
+  field :lastname, type: String
+  field :specialty, type: String
+  field :qualifications, type: String
+  field :experience, type: Integer
+  field :education, type: String
+  field :additional_info, type: String
 end
