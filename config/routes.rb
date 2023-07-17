877dbb1a5d90b123d7068b2eb0b1255ae731cc26
muellerviana <muellerviana@gmail.com>
2023-07-17 19:55:00 -0300

    add doctor crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'doctors/index'
   resources :consumers
+  resources :doctors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
